ba5966610cd50d2e5f615f330f6258eca4a610fb
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2024-08-14 17:24:37 +0700

    new update

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,10 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
